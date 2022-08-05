f3a03f224063bdac9ed3b6c0805acd858aeeb4e1
Krista Gartland <Randomflyingobject@yahoo.com>
2022-08-04 19:46:52 -0500

    Studio 6 added

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,43 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+   @Test
+    public void singleLetterReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[a]"));
+   }
+
+   @Test
+    public void oneBracketShouldFail() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[}"));
+   }
+
+   @Test
+    public void InsideBalancedBracketTest() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Ins[ide]"));
+   }
+
+   @Test
+    public void LotsOfBracketsTest() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[[]]]]]]]]]"));
+   }
+
+   @Test
+    public void TestNumbersInString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[4444]"));
+   }
+
+   @Test
+    public void ConcurrentBracketsTest() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][]"));
+   }
+
+   @Test
+    public void TestSymbolsBeingAdded() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[%%%%%%&&&&&&]"));
+   }
 }
